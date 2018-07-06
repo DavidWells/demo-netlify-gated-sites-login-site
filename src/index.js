f1e65a0ccc59cba3e30963795340f53d7edaa5af
DavidWells <hello@davidwells.io>
2018-07-06 11:17:29 -0700

    log ref

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+console.log('document.referrer', document.referrer)
+
 ReactDOM.render(<App />, document.getElementById('root'));
