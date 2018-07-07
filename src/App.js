cd86c841fb43ceb41a894fafe88ecf1b020d6dd2
DavidWells <hello@davidwells.io>
2018-07-06 19:13:57 -0700

    parse params

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import queryString from 'query-string'
 import netlifyIdentity from 'netlify-identity-widget'
 import logo from './logo.svg'
 import './App.css'
@@ -13,7 +12,7 @@ export default class App extends Component {
     netlifyIdentity.init()
   }
   componentDidMount() {
-    const parsed = queryString.parse(window.location.search)
+    const parsed = getParams()
     // Set redirect URL
     if (parsed.site) {
       localStorage.setItem(REDIRECT_URL, parsed.site)
@@ -72,4 +71,39 @@ export default class App extends Component {
       </div>
     )
   }
+}
+
+function getParams(url) {
+  const urlParams = {}
+  const pattern = /([^&=]+)=?([^&]*)/g
+  let params
+  let matches
+  if (url) {
+    const p = url.match(/\?(.*)/) // query
+    params = (p && p[1]) ? p[1].split('#')[0] : ''
+  } else {
+    params = window.location.search.substring(1)
+  }
+  if (!params) return false
+  while (matches = pattern.exec(params)) { // eslint-disable-line
+    if (matches[1].indexOf('[') == '-1') { // eslint-disable-line
+      urlParams[decode(matches[1])] = decode(matches[2])
+    } else {
+      const b1 = matches[1].indexOf('[')
+      const aN = matches[1].slice(b1 + 1, matches[1].indexOf(']', b1))
+      const pN = decode(matches[1].slice(0, b1))
+
+      if (typeof urlParams[pN] !== 'object') {
+        urlParams[decode(pN)] = {}
+        urlParams[decode(pN)].length = 0
+      }
+
+      if (aN) {
+        urlParams[decode(pN)][decode(aN)] = decode(matches[2])
+      } else {
+        Array.prototype.push.call(urlParams[decode(pN)], decode(matches[2]))
+      }
+    }
+  }
+  return urlParams
 }
