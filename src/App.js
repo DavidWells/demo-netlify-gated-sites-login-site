f323630a38472f4b5df1b4f4f3076da92c3e1b03
DavidWells <hello@davidwells.io>
2018-07-06 19:01:54 -0700

    add button

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import './App.css'
 
 const REDIRECT_URL = 'redirect_url'
 
-class App extends Component {
+export default class App extends Component {
   constructor() {
     super()
     // start identity
     netlifyIdentity.init()
   }
   componentDidMount() {
-    const parsed = queryString.parse(location.search)
+    const parsed = queryString.parse(window.location.search)
     // Set redirect URL
     if (parsed.site) {
       localStorage.setItem(REDIRECT_URL, parsed.site)
     }
-     /* Register listeners on identity widget events */
+    /* Register listeners on identity widget events */
     netlifyIdentity.on("login", (user) => {
       /* Close netlify identity modal on login */
       netlifyIdentity.close()
@@ -29,7 +29,9 @@ class App extends Component {
       // window.location.href = window.location.href
     })
     netlifyIdentity.on("logout", () => {
-      this.setState({ loggedIn: false })
+      // clear redirectURL
+      localStorage.removeItem(REDIRECT_URL)
+      // reload page
       window.location.href = window.location.href
     })
   }
@@ -37,6 +39,25 @@ class App extends Component {
     // Open login
     netlifyIdentity.open()
   }
+  handleLogOut = () => {
+    netlifyIdentity.logout()
+  }
+  renderButton() {
+    const user = netlifyIdentity.currentUser()
+    if (!user) {
+      return (
+        <button onClick={this.handleLogIn}>
+          Sign up or Log in
+        </button>
+      )
+    }
+    return (
+      <button onClick={this.handleLogOut}>
+        Log out { user.email }
+      </button>
+    )
+  }
+
   render() {
     return (
       <div className="App">
@@ -44,14 +65,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Login Site</h1>
         </header>
+        <br />
         <div>
-         <button onClick={this.handleLogIn}>
-           Log in with netlify
-         </button>
+         {this.renderButton()}
        </div>
       </div>
     )
   }
 }
-
-export default App
